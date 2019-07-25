b70e7dbca2798019819cb5dc55aa27d0a52a968b
Victor Shabanov <shabanov.dev.git@gmail.com>
2019-07-25 23:44:00 +0300

    Initial commit

---
+++ Example/StorageKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  StorageKit
+//
+//  Created by Victor Shabanov on 07/25/2019.
+//  Copyright (c) 2019 Victor Shabanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
